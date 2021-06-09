fe4bdf30bbf49346747ac16132315ef0feb3f2e4
Sarah <sarah.caze@yahoo.com>
2021-06-09 11:14:42 +0200

    Demande / Entite / Message / Views (ControllerRest et model)

--- ShareYourFood/src/main/java/sopra/ShareYourFood/model/Views.java
+++ ShareYourFood/src/main/java/sopra/ShareYourFood/model/Views.java
@@ -1,5 +1,15 @@
 package sopra.ShareYourFood.model;
 
 public class Views {
+	
 	public static class ViewCommon {}
+	
+	public static class ViewMessage extends ViewCommon{}
+	public class ViewMessageWithDemande extends ViewMessage{}
+	
+	public static class ViewEntite extends ViewCommon{}
+	
+	public static class ViewDemande extends ViewCommon{}
+	public class ViewDemandeWithLot extends ViewDemande{}
+	public class ViewDemandeWithEntite  extends ViewDemande{}
 }
